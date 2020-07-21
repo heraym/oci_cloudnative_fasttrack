863abb08b9f52d418c80bb1b09f58a56224a2c96
Hernan Aymard <heraym@hotmail.com>
2020-07-21 15:49:09 -0300

    setup java

--- setup/setupHelidon.sh
+++ setup/setupHelidon.sh
@@ -5,7 +5,7 @@ tar -xvzf ~/java/openjdk-14.0.2_linux-x64_bin.tar.gz
 #Install Maven
 mkdir ~/maven
 wget https://downloads.apache.org/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz -P /tmp
-tar xf /tmp/apache-maven-3.6.0-bin.tar.gz -C ~/maven
+tar xf /tmp/apache-maven-3.6.3-bin.tar.gz -C ~/maven
 
 #Env Variables
 sudo cp helidon.sh /etc/profile.d/
